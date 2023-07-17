d33bd70fa747ce74d9bf6fa3d1b469282a649388
Daniil <danilcursin@mail.ru>
2023-07-17 16:22:17 +0300

    0.2 create models, add some information in db

---
+++ service/services/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from services.models import Servise, Plan, Subscription
+
+admin.site.register(Servise)
+admin.site.register(Plan)
+admin.site.register(Subscription)
